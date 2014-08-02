e69339861094ecd70dd446b00f232c89fb2c4af8
sectore <info@websector.de>
2014-08-02 14:59:21 +0200

    adding mock factory

---
+++ src/helper/mock-factory.js
@@ -0,0 +1,54 @@
+module.exports = {
+
+  createAlbumsPreferences: function () {
+    return {
+      loop: true
+    }
+  },
+
+  createAlbums: function (noAlbums, noSlides) {
+    var i = 0,
+      max = noAlbums || 1,
+      maxSlides = noSlides || Math.round(Math.random() * 10) + 1,
+      albums = [];
+
+    for (i; i < max; i++) {
+      albums[i] = this.createAlbum(i, maxSlides);
+    }
+
+    return albums;
+  },
+
+
+  createAlbum: function (id, noSlides) {
+    var maxSlides = noSlides || Math.round(Math.random() * 10) + 1;
+
+    return {
+      slidePath: 'slides/album' + id + '/',
+      thumbsPath: 'thumbs/album' + id + '/',
+      description: "album description " + id,
+      slides: this.createSlides(maxSlides)
+    }
+  },
+
+  createSlides: function (noSlides) {
+    var i = 0,
+      max = noSlides || 1,
+      slides = [];
+
+    for (i; i < max; i++) {
+      slides[i] = this.createSlide(i);
+    }
+
+    return slides;
+  },
+
+  createSlide: function (id) {
+    return {
+      name: id + '.jpg',
+      thumbName: 'thumb_' + id + '18.jpg',
+      description: "slide description " + id
+    }
+  }
+
+}
