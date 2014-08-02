50af0cff0fe95e5c4e1a5b4ae2c042cf4b4e1159
sectore <info@websector.de>
2014-08-02 10:56:09 +0200

    handling of nextSlide() updated to check loop option, too

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@
 var Vue = require('vue'),
   request = require('superagent'),
   util = require('./util'),
+  error = require('./error'),
 
   WSSlideshow = {
 
@@ -32,9 +33,12 @@ var Vue = require('vue'),
         },
 
         template: require('./main.html'),
-
+        // default data
         data: {
           loading: true,
+          albumsPreferences: {
+            loop: false
+          },
           albums: [],
           preferences: {
             sec: 4,
@@ -73,33 +77,58 @@ var Vue = require('vue'),
           // albums
           // ------------------------------------------------------------
 
-          currentAlbum: function(){
-            var album = this.albums[this.albumIndex];
-            if(!album){
-              album = {slides: []};
-            }
-            return album;
+
+          nullAlbum: function () {
+            return {
+              slides: []
+            };
           },
-          hasNextAlbum: function(){
+
+          currentAlbum: function () {
+            return this.albums[this.albumIndex] || this.nullAlbum;
+          },
+
+          hasNextAlbum: function () {
             return this.albumIndex < this.albums.length - 1;
           },
-          hasPrevAlbum: function(){
+
+          hasPrevAlbum: function () {
             return this.albumIndex > 0;
           },
 
           // slides
           // ------------------------------------------------------------
 
-          currentSlide: function(){
-            return this.currentAlbum.slides[this.slideIndex];
+          nullSlide: function () {
+            return {}
           },
 
-          hasNextSlide: function(){
-            return this.slideIndex < this.currentAlbum.slides.length - 1;
+          currentSlide: function () {
+            return this.currentAlbum.slides[this.slideIndex] || this.nullSlide;
           },
-          hasPrevSlide: function(){
+
+          hasNextSlide: function () {
+            var result = this.currentAlbum
+              && this.currentAlbum.slides
+              && this.slideIndex < this.currentAlbum.slides.length - 1;
+
+            // check loop option, too
+            if (!result) {
+              result = !!this.albumsPreferences.loop || !!this.currentAlbum.loop
+            }
+
+            return result;
+          },
+
+          hasPrevSlide: function () {
             return this.slideIndex > 0;
+          },
+
+
+          maxSlideIndex: function () {
+            return this.currentAlbum.slides.length - 1;
           }
+
         },
 
         methods: {
@@ -112,10 +141,11 @@ var Vue = require('vue'),
             var self = this;
             request.get(url, function (err, data) {
               if (err) {
-                // TODO: handle error
+                error.show("Error loading JSON file", err);
               } else {
                 var json = data.body;
                 self.preferences = util.mergeObject(self.preferences, json.preferences);
+                self.albumsPreferences = util.mergeObject(self.albumsPreferences, json.albumsPreferences);
                 self.albums = json.albums;
                 self.loading = false;
               }
@@ -126,15 +156,15 @@ var Vue = require('vue'),
           // albums
           // ------------------------------------------------------------
 
-          nextAlbum: function(){
-            if(!!this.hasNextAlbum){
+          nextAlbum: function () {
+            if (!!this.hasNextAlbum) {
               this.albumIndex += 1;
               this.slideIndex = 0;
             }
           },
 
-          prevAlbum: function(){
-            if(!!this.hasPrevAlbum){
+          prevAlbum: function () {
+            if (!!this.hasPrevAlbum) {
               this.albumIndex -= 1;
             }
           },
@@ -142,19 +172,33 @@ var Vue = require('vue'),
           // slides
           // ------------------------------------------------------------
 
-          nextSlide: function(){
-            if(!!this.hasNextSlide){
-              this.slideIndex += 1;
-            } else {
-              this.nextAlbum();
+          /**
+           * Triggers next slide index.
+           * If not a next slide is available
+           * it checks the loop options of current album
+           * or all albums, too.
+           */
+          nextSlide: function () {
+            if (!!this.hasNextSlide) {
+              // check max slide index
+              if (this.slideIndex == this.maxSlideIndex) {
+                if (!!this.currentAlbum.loop) {
+                  this.slideIndex = 0;
+                } else if (!!this.albumsPreferences.loop) {
+                  this.slideIndex = 0;
+                  this.nextAlbum();
+                }
+              } else {
+                this.slideIndex += 1;
+              }
             }
           },
 
-          prevSlide: function(){
-            if(!!this.hasPrevSlide){
+          prevSlide: function () {
+            if (!!this.hasPrevSlide) {
               this.slideIndex -= 1;
             } else {
-              if(!!this.hasPrevAlbum){
+              if (!!this.hasPrevAlbum) {
                 this.prevAlbum();
                 this.slideIndex = this.currentAlbum.slides.length - 1;
               }
