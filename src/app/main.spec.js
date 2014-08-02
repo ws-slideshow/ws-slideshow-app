bbc6d943bb831624dc788d99dcdc5d8bf33ae2bd
sectore <info@websector.de>
2014-08-02 13:27:39 +0200

    removing log statements

--- src/app/main.spec.js
+++ src/app/main.spec.js
@@ -17,8 +17,6 @@ describe('main', function() {
   describe('options', function() {
 
     it('json path has to be defined by default ', function() {
-      console.log("JSON");
-      console.log(component.json);
       expect(component.json).to.equal('./slides.json');
     });
   });
@@ -26,8 +24,6 @@ describe('main', function() {
   describe('albums', function() {
 
     it('currentAlbum ', function() {
-      console.log("component.options");
-      console.log(component.options);
       expect(component.currentAlbum).to.deep.equal(component.nullAlbum);
     });
   });
